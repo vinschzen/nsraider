996d8b0fac61a741909f22ca353620faaba0ec5b
vinshent <75918051+LyfelessVin@users.noreply.github.com>
2023-07-10 09:52:53 +0700

    simon says

--- Assets/AutoHand/Scripts/Demo/Text Demo/SliderTextChanger.cs
+++ Assets/AutoHand/Scripts/Demo/Text Demo/SliderTextChanger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9a8a2db06a19ec64042e4bbb95150191e8be1e715830f7a7fba5fd64f418c5
-size 368
+oid sha256:edcec75a05fb895e080195e640048ed29d7b2d6efbc9ae51993c16d50ae1b5ce
+size 378
---
+++ Assets/Low Poly Cave Pack/Scenes/SimonSays.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ec86c5899a28affd54a601cbe6eee75aaf34f2df0eac73494c6c22c5d2d540c
+size 4125
